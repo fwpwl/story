2f2748a7ab126b2c2929ae8bf75187f130203b12
fwp <fwp@myhost.local>
2020-06-10 11:41:54 +0800

    init project

--- xlyz/xlyz/settings.py
+++ xlyz/xlyz/settings.py
@@ -81,9 +81,9 @@ WSGI_APPLICATION = 'xlyz.wsgi.application'
 DATABASES = {
     "default": {
             "ENGINE": "django.db.backends.postgresql_psycopg2",
-            "NAME": "xlyz",
+            "NAME": "story",
             "USER": "root",
-            "PASSWORD": "",
+            "PASSWORD": "root",
             "HOST": "127.0.0.1", 
             "PORT": "5432"
         }
